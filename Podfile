65e31afd8f907e3c44f9db7c6dd983e39cc598f3
Craig Thomas <Craig@Craigs-MacBook-Pro.local>
2017-06-24 10:28:43 +0200

    Initial commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '11.0'
+inhibit_all_warnings!
+use_frameworks!
+
+
+def application_pods
+    pod 'SnapKit', :git => 'https://github.com/SnapKit/SnapKit.git', :branch => 'swift-4'
+    pod 'SwiftyJSON', '3.1.4'
+end
+
+target 'WeatherCloud' do
+    application_pods
+end
---
+++ WeatherCloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherCloud
+//
+//  Created by Craig Thomas on 2017/06/24.
+//  Copyright © 2017 CraigThomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
